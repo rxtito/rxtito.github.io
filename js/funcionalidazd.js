88ab58e4f364e010159d2caf141d91abb4dca40c
rxtito <126334277+rxtito@users.noreply.github.com>
2023-02-25 11:15:59 -0300

    initial commit

---
+++ js/funcionalidazd.js
@@ -0,0 +1,17 @@
+
+
+function aparecercv(){
+    
+   
+        document.getElementById("contenedorjs").style.display="block";
+        
+}
+
+function ocultarcv(){
+    
+   
+    document.getElementById("contenedorjs").style.display="none";
+    
+}
+    
+
